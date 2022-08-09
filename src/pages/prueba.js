f1b891272be2e72b578f9893258bfb764aa7db5e
Alicida <brayantbola@gmail.com>
2022-08-09 01:26:57 -0500

    add branches

--- src/pages/prueba.js
+++ src/pages/prueba.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 export default function prueba() {
   return (
     <Layout>
-        <div><h1>Prueba</h1></div>
+        <div><h1>Prueba de staging 2</h1></div>
     </Layout>
   )
 }
