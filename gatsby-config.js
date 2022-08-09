1c5fb93eabf65bb3b572a449986b21ee95c0fea4
Alicida <brayantbola@gmail.com>
2022-08-08 19:38:54 -0500

    start up

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    'gatsby-plugin-offline',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/pages/prueba.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function prueba() {
+  return (
+    <Layout>
+        <div><h1>Prueba</h1></div>
+    </Layout>
+  )
+}
